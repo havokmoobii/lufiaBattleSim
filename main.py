5848bfba607305566bedc3e8886c74dd470b07ef
HavokMoobii <jasonmcounter@gmail.com>
2025-05-13 20:13:09 -0500

    Set FPS to 60

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Starting Battle Simulator!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
