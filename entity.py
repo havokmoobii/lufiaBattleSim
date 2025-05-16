cc08b7d055f47a825cfb834502efb29e772564c4
HavokMoobii <jasonmcounter@gmail.com>
2025-05-15 20:53:02 -0500

    Maxim can now move aligned with the grid in all directions

--- entity.py
+++ entity.py
@@ -1,17 +1,25 @@
 import pygame
+from enum import Enum
+
+class Direction(Enum):
+    UP = "up",
+    LEFT = "left",
+    DOWN = "down",
+    RIGHT = "right"
 
 class Entity(pygame.sprite.Sprite):
 
     def __init__(self, color, width, height, x, y):
         pygame.sprite.Sprite.__init__(self)
 
-        self.sprite_sheet = pygame.image.load("./sprites/maxim.png").convert_alpha()
+        self.width = width
+        self.height = height
 
-        self.image = pygame.Surface([width, height], pygame.SRCALPHA)
+        self.sprite_sheet = pygame.image.load("sprites/maxim.png").convert_alpha()
 
-        self.image.get_rect()
-
+        self.moving = False
         self.move_frame = 0
+        self.move_direction = Direction.DOWN
 
         self.x = x
         self.y = y
@@ -19,27 +27,71 @@ class Entity(pygame.sprite.Sprite):
     def draw(self, screen):
         # sub-classes must override
 
-        if self.move_frame == 31:
-            self.move_frame = 0            
-        else:
-            self.move_frame += 1
+        self.image = pygame.Surface([self.width, self.height], pygame.SRCALPHA)
 
-        if self.move_frame > 15:
-            self.image.blit(self.sprite_sheet, (0,0), (22,5,16,26))
+        match(self.move_direction):
+            case Direction.UP:
+                sprite_sheet_x_offest = 78
+            case Direction.LEFT:
+                sprite_sheet_x_offest = 41
+            case Direction.DOWN:
+                sprite_sheet_x_offest = 5
+            case Direction.RIGHT:
+                sprite_sheet_x_offest = 41
+
+        if self.moving:
+            if self.move_frame <= 0:
+                self.moving = False
+                self.move_frame = 0
+            else:
+                self.move_frame -= 2
+
+        if self.move_frame == 0 or self.move_frame % 16 > 8:
+            self.image.blit(self.sprite_sheet, (0,0), (4,sprite_sheet_x_offest,16,26))
         else:
-            self.image.blit(self.sprite_sheet, (0,0), (4,5,16,26))
+            self.image.blit(self.sprite_sheet, (0,0), (22,sprite_sheet_x_offest,16,26))
+
+        if self.move_direction == Direction.LEFT:
+            self.image = pygame.transform.flip(self.image, True, False)
 
         screen.blit((pygame.transform.scale(self.image, (32, 52))), (self.x, self.y))
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_w]:
-            self.y -= 4
-        if keys[pygame.K_a]:
-            self.x -= 4
-        if keys[pygame.K_s]:
-            self.y += 4
-        if keys[pygame.K_d]:
-            self.x += 4
+        if self.moving:
+            match(self.move_direction):
+                case Direction.UP:
+                    self.y -= 4
+                case Direction.LEFT:
+                    self.x -= 4
+                case Direction.DOWN:
+                    self.y += 4
+                case Direction.RIGHT:
+                    self.x += 4
+
+        if not self.moving:
+            if keys[pygame.K_w]:
+                self.move_direction = Direction.UP
+                self.moving = True
+                self.move_frame = 16
+        if not self.moving:
+            if keys[pygame.K_s]:
+                self.move_direction = Direction.DOWN
+                self.moving = True
+                self.move_frame = 16
+        if not self.moving:
+            if keys[pygame.K_a]:
+                self.move_direction = Direction.LEFT
+                self.moving = True
+                self.move_frame = 16
+        if not self.moving:
+            if keys[pygame.K_d]:
+                self.move_direction = Direction.RIGHT
+                self.moving = True
+                self.move_frame = 16
+            
+        
+        
+            
         
--- main.py
+++ main.py
@@ -8,14 +8,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    maxim = Entity("white", 16, 26, SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    maxim = Entity("white", 16, 26, SCREEN_WIDTH/2 - 28, SCREEN_HEIGHT/2 - 8)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("pink")
+        full_map = pygame.image.load("maps/02SecretSkillsCave.png").convert_alpha()
+        room = pygame.Surface([253, 227], pygame.SRCALPHA)
+        room.blit(full_map, (0,0), (326,664,240,224))
+            
+        screen.fill("#080800")
+        screen.blit((pygame.transform.scale(room, (room.get_width() * 2, room.get_height() * 2))), (SCREEN_WIDTH/2 - room.get_width(), SCREEN_HEIGHT/2 - room.get_height()))
         maxim.draw(screen)
         maxim.update(dt)
 
